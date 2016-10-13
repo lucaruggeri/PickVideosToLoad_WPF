2492ff50bbc1fa17ec5ef5cb31928650a3f7e1e3
lucaruggeri <lucaruggeri@hotmail.com>
2016-10-13 22:32:16 +0200

    First commit

---
+++ PickVideosToLoad/PickVideosToLoad/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Utilities;
+
+namespace PickVideosToLoad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSetSourceFolder_Click(object sender, RoutedEventArgs e)
+        {
+            lblSourceFolder.Content = Utilities.WpfClassicDialogs.OpenFolderDialog(@"G:\VIDEOS\Shark Tank Season 1-5\Shark.Tank.Season.5");
+        }
+
+        private void btnSetDestinationFolder_Click(object sender, RoutedEventArgs e)
+        {
+            lblDestinationFolder.Content = Utilities.WpfClassicDialogs.OpenFolderDialog(@"C:\_TEMP");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ResetGraphics();
+        }
+
+        private void ResetGraphics()
+        {
+            lblSourceFolder.Content = string.Empty;
+            lblDestinationFolder.Content = string.Empty;
+        }
+
+    }
+}
---
+++ PickVideosToLoad/PickVideosToLoad/Utilities/WpfClassicDialogs/WpfClassicDialogs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms; //must add reference to System.Windows.Forms
+
+namespace Utilities
+{
+    public static class WpfClassicDialogs
+    {
+
+        public static string OpenFolderDialog(string defaultPath)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+
+            dialog.Description = "My dialog";
+            dialog.SelectedPath = defaultPath;
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                return dialog.SelectedPath;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
